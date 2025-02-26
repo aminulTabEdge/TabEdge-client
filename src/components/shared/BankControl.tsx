d81ce024ce33623e9ea4cb88def8690fb7aa3760
Aminul <aminultabedge@gmail.com>
2025-02-26 19:34:31 +0600

    Folder change to shared

--- src/components/shared/BankControl.tsx
+++ src/components/shared/BankControl.tsx
@@ -9,7 +9,6 @@ const BankControl = () => {
     <SectionContainer tailwindClass="bg-gradient-to-r from-blue-50 to-blue-100">
       <div className="container mx-auto px-4">
         <SectionHeading title="TabEdge Bank" />
-
         {/* Responsive Grid Layout */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
           {tabEdgeBank.map((service) => (
