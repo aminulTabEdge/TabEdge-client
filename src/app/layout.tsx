f823b450f3a24dab7f6d07060207c239ed6dabab
Aminul <aminultabedge@gmail.com>
2025-03-05 16:55:54 +0600

    Layout meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,15 @@ export const metadata: Metadata = {
   title: "TabEdge",
   description:
     "TabEdge is your go-to digital banking platform offering seamless online banking solutions, virtual credit cards, prepaid Visa cards, and international debit cards tailored for global users. With TabEdge, open a free checking account, access a virtual Visa card instantly, and enjoy secure, worldwide transactions with trusted partners like Visa and Mastercard. Whether you’re managing your money online, looking for the best virtual debit card, or wondering how to get a virtual credit card, TabEdge makes financial freedom easy. Trusted by digital bank users globally, we rival services like Chime, Revolut, N26, and more. Simplify your finances with TabEdge today",
+  keywords:
+    "TabEdge, TabEdge Banking, TabEdge cross border transaction, Secure USD payment",
+  openGraph: {
+    title: "TabEdge Banking",
+    description: "Discover the best services with us.",
+    type: "website",
+    url: "https://tabedge.com",
+    siteName: "TabEdge",
+  },
 };
 
 export type ChildrenType = {
